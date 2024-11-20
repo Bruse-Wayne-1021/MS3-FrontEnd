5f629cc293f3661b13af68c334b6998b52fca9c8
Sajilan.P <ut01261tic2024@gmail.com>
2024-11-20 12:42:52 +0530

    login1'

--- LMS-Angular/src/app/Service/user.service.ts
+++ LMS-Angular/src/app/Service/user.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LToken } from '../Models/Token';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +13,21 @@ export class UserService {
 
    }
 
-   
 
-   login(){
 
-   }
+   login(user :any) {
+    return this.http.post<LToken>("http://localhost:5255/api/Login/login", user);
+  }
+
+  IsLoggedIn(){
+    if(localStorage.getItem('Token')){
+      const token=localStorage.getItem('token');
+      if(token){
+        const decoded:any=jwtDecode(token);
+        localStorage.setItem('user',JSON.stringify(decoded))
+      }
+      return true;
+    }
+    return false;
+  }
 }
---
+++ LMS-Angular/src/app/components/Admin/nev-bar/nev-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nev-bar',
+
+  templateUrl: './nev-bar.component.html',
+  styleUrl: './nev-bar.component.css'
+})
+export class NevBarComponent {
+
+}
